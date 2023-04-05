7968326d153b6ae4f83180a0d641e7ab76267475
Mark C Allen <mark@markcallen.com>
2023-04-05 00:01:55 +0000

    Added repo to make this dynamic

--- generate-release-notes.sh
+++ generate-release-notes.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+#
+
+REPO=$(git config --get remote.origin.url | grep -o -P '(?<=:).*(?=.git)')
 
 LAST=$(curl -s -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/markcallen/continuous-delivery/releases | jq -r .[0].tag_name)
+  https://api.github.com/repos/${REPO}/releases | jq -r .[0].tag_name)
 
 LATEST=$1
 
-echo **Full Changelog**: https://github.com/markcallen/continuous-delivery/compare/${LAST}...${LATEST}
+echo **Full Changelog**: https://github.com/${REPO}/compare/${LAST}...${LATEST}
 
 git log --pretty="- %s" ${LAST}..HEAD
